24bd181552cf7d517140dcdb395d014fcaf40ffa
NicholasDollick <nidollick@gmail.com>
2019-06-02 16:22:55 -0700

    starting to migrate colors to dictionary

---
+++ QNTMWPFUserInterface/Helpers/SentByMeToBackgroundConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace QNTMWPFUserInterface.Helpers
+{
+    // Takes a boolean input to check if message was sent by user to apply correct background color to message
+    public class SentByMeToBackgroundConverter : BaseValueConverter<SentByMeToBackgroundConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Application.Current.FindResource("MessageSentBrush") : Application.Current.FindResource("MessageRecievedBrush");
+        }
+
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
